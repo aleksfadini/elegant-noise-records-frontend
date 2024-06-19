b554dc891e10f6a6c019db1478f98f3db982463c
aleksfadini <aleksfadini@gmail.com>
2024-06-18 23:41:01 -0400

    fix database need to square images

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,9 +6,13 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Raleway+Dots&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&display=swap"
             rel="stylesheet"
           />
+          {/* <link
+            href="https://fonts.googleapis.com/css2?family=Raleway+Dots&display=swap"
+            rel="stylesheet"
+          /> */}
           {/* <link
             href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&display=swap"
             rel="stylesheet"
